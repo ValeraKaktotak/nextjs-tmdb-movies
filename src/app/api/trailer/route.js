5bf69c136430b47fbe3bee7362f7bd14014914de
Valera <valerakaktotak@gmail.com>
2024-06-23 23:42:01 +0300

    created trailer api POST request

---
+++ src/app/api/trailer/route.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import { NextResponse } from 'next/server'
+
+const key = process.env.API_KEY
+
+export async function POST(req) {
+  try {
+    const { media_type, id } = await req.json()
+    const response = await axios.get(
+      `https://api/themoviedb.org/3/${media_type}/${id}/videos?api_key=${key}&language=en-US`
+    )
+    const data = response.data
+    const trailer = data.results.find(
+      (item) => item.type === 'Trailer' && item.site === 'Youtube'
+    )
+    const trailerKey = trailer ? trailer.key : null
+    return NextResponse.json({ trailerKey }, { status: 200 })
+  } catch (err) {
+    return NextResponse.json(
+      { error: 'Failed to fetch a trailer' },
+      { status: 500 }
+    )
+  }
+}
--- src/components/Trending/Trending.jsx
+++ src/components/Trending/Trending.jsx
@@ -1,5 +1,42 @@
+import axios from 'axios'
+import { useState } from 'react'
+
 const Trending = ({ trendingData, loading }) => {
-  console.log(trendingData)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [itemsPerPage, setItemsPerPage] = useState(15)
+  const [open, setOpen] = useState(false)
+  const [selectedTitle, setSelectedTitle] = useState(null)
+  const [trailerKey, setTrailerKey] = useState(null)
+  const [showTrailer, setShowTrailer] = useState(false)
+
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value)
+  }
+  const handleClickOpen = (title) => {
+    setSelectedTitle(title)
+    setOpen(true)
+    setShowTrailer(false)
+    setTrailerKey(null)
+  }
+  const handleClickClose = (title) => {
+    setSelectedTitle(null)
+    setOpen(false)
+    setShowTrailer(null)
+    setTrailerKey(false)
+  }
+  const handlePlayTrailer = async (mediaType, id) => {
+    try {
+      const response = await axios.post('/api/trailer', {
+        media_type: mediaType,
+        id
+      })
+      setTrailerKey(response.data.trailerkey)
+      setShowTrailer(true)
+    } catch (error) {
+      console.log('Failed to fetch Trailer', error)
+    }
+  }
+
   return <div>Trending</div>
 }
 
