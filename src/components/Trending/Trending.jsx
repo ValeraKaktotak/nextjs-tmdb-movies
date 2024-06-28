7298363523aa729321f9a3578964fdd8eae5503c
Valera <valerakaktotak@gmail.com>
2024-06-28 14:10:18 +0300

    done trending component

--- src/components/Trending/Trending.jsx
+++ src/components/Trending/Trending.jsx
@@ -1,8 +1,18 @@
 'use client'
-import { Badge, Pagination, Stack } from '@mui/material'
+import PlayArrowIcon from '@mui/icons-material/PlayArrow'
+import {
+  Badge,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Pagination,
+  Stack
+} from '@mui/material'
 import axios from 'axios'
 import Image from 'next/image'
 import { useState } from 'react'
+import YouTube from 'react-youtube'
 
 const Trending = ({ trendingData, loading }) => {
   const [currentPage, setCurrentPage] = useState(1)
@@ -37,7 +47,7 @@ const Trending = ({ trendingData, loading }) => {
         media_type: mediaType,
         id
       })
-      setTrailerKey(response.data.trailerkey)
+      setTrailerKey(response.data.trailerKey)
       setShowTrailer(true)
     } catch (error) {
       console.log('Failed to fetch Trailer', error)
@@ -83,7 +93,6 @@ const Trending = ({ trendingData, loading }) => {
                   alt={trending.original_title || trending.title}
                   width={500}
                   height={500}
-                  layout='responsive' // Makes the image responsive
                   priority={true} // High-priority image, preload
                 />
                 <h1 className='text-center md:font-medium md:text-sm hover:text-red-500 transition duration-300'>
@@ -109,10 +118,81 @@ const Trending = ({ trendingData, loading }) => {
             page={currentPage}
             variant='outlined'
             color='primary'
-            className='pagination custom'
           />
         </Stack>
       </div>
+      <Dialog
+        open={open}
+        onClose={handleClickClose}
+        maxWidth='sm'
+        fullWidth={true}
+        className='rounded-lg p-4'
+        PaperProps={{
+          className: 'bg-white dark:bg-gray-800 text-black dark:text-white'
+        }}
+      >
+        <DialogTitle
+          className={
+            selectedTitle?.media_type === 'tv'
+              ? 'text-[3.5vmin] font-semibold text-left'
+              : 'text-[3.5vmin] font-semibold text-left'
+          }
+        >
+          {selectedTitle?.original_title ||
+            selectedTitle?.title ||
+            selectedTitle?.original_name ||
+            selectedTitle?.name}
+        </DialogTitle>
+        <DialogContent className='flex flex-col items-center'>
+          {selectedTitle && (
+            <>
+              {selectedTitle.media_type === 'tv' ? (
+                <>
+                  <Image
+                    src={`https://image.tmdb.org/t/p/original${selectedTitle.poster_path}`}
+                    alt={selectedTitle.original_title || selectedTitle.title}
+                    width={500}
+                    height={500}
+                    layout='responsive'
+                    priority={true}
+                    className='object-contain max-h-[500px]'
+                  />
+                  <div style={{ maxHeight: '200px', overflowY: 'auto' }}>
+                    <p className='p-4'>{selectedTitle.overview}</p>
+                  </div>
+                </>
+              ) : (
+                <>
+                  {/* Show trailer for movies */}
+                  {trailerKey ? (
+                    <YouTube
+                      videoId={trailerKey}
+                      className='w-full h-full flex justify-center text-center'
+                    />
+                  ) : (
+                    <IconButton
+                      color='primary'
+                      onClick={() =>
+                        handlePlayTrailer(
+                          selectedTitle.media_type,
+                          selectedTitle.id
+                        )
+                      }
+                      aria-label='play trailer'
+                      className='m-4'
+                    >
+                      <PlayArrowIcon />
+                    </IconButton>
+                  )}
+                  <div style={{ maxHeight: '200px', overflowY: 'auto' }}>
+                    <p className='p-4'>{selectedTitle.overview}</p>
+                  </div>
+                </>
+              )}
+            </>
+          )}
+        </DialogContent>
+      </Dialog>
     </>
   )
 }
